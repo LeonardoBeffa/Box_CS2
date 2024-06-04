f8f7c4aba951d68d94d0aa480f70033197aacb94
LeonardoBeffa <leonardorbeffa@gmail.com>
2024-06-04 12:59:53 -0300

    Update Caixas_cs2_web.py

--- Caixas_cs2_web.py
+++ Caixas_cs2_web.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 os.system('cls')
 
-diretorio = 'C:/Users/Beffa/Documents/Python/webScraping/Steam/'
+diretorio = 'Diretorio de salvamento e leitura do arquivo.csv'
 
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
 url_case = 'https://csgostash.com/containers/skin-cases'
